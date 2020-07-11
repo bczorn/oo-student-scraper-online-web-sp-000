ea9cc91c83f4b4aaaa8b7a66c4db95d13e5fa866
Becky Zorn <bczorn@gmail.com>
2020-07-11 01:14:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send("#{key.to_s}=", value)
+      @@all << self
+    end
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |x|
+      self.new(x)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+      x = Scraper.scrape_profile_page
+      Scraper.scrape_profile_page.send(attributes_hash)
+      
   end
 
   def self.all
-    
+    @@all
   end
 end
 
